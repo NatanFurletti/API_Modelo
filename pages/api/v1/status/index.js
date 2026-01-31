3ff037ae4d0b91db5f18a96b05304267e1df4765
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-01-31 01:47:02 +0000

    fix: `absolute imports`.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import { Query } from "pg";
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   response.status(200).json({ status: "OK" });
 }
 
