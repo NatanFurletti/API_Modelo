87d622940fb42d200c03295ffe982e2402a2ffa6
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-02-01 14:33:01 +0000

    fix: frase in ´pages/index.js´

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 
 export default function Home() {
-  const pergunta = "Peteca amanha??🥹";
+  const pergunta = "Petequinha??🥹";
 
   const [scale, setScale] = useState(1);
   const [started, setStarted] = useState(false);
