633c6da0d6a3bf407db0934dd3a92719787dd099
Natan Furletti <natan.furletti@outlook.com>
2026-02-02 19:35:34 -0300

    fix: `jest`?

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ status: "OK" });
+  response.status(200).json();
 }
 
 export default status;
