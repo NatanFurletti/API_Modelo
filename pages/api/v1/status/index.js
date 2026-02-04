5e44223a1236ba12a2edc9b11308145894dc99d1
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-02-04 13:02:56 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,20 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  const databaseName = process.env.POSTGRES_DB;
+
   const updatedAt = new Date().toISOString();
+
   const databaseVersion = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersion.rows[0].server_version;
 
   const databaseMaxConnections = await database.query("SHOW max_connections;");
   const databaseMaxConnectionsValue =
     databaseMaxConnections.rows[0].max_connections;
-  const openedConnections = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db' AND state = 'active';",
-  );
+  const openedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
   const databaseOpenedConnectionsValue = openedConnections.rows[0].count;
 
   response.status(200).json({
