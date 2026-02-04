928e48c472de5d5b90a5b6d1c25ab544fbc463a3
Natan Furletti <128628722+NatanFurletti@users.noreply.github.com>
2026-02-04 03:51:52 +0000

    add:database version,connecions querys and tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,9 +2,26 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue =
+    databaseMaxConnections.rows[0].max_connections;
+  const openedConnections = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db' AND state = 'active';",
+  );
+  const databaseOpenedConnectionsValue = openedConnections.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
-
 export default status;
